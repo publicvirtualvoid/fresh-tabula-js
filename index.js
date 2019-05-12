fe6aa914b4fe6d18d965e8dd6a9a6e834c32f6e8
cdtinney <colintinney@gmail.com>
2019-05-12 13:18:54 -0700

    style: cleanup

--- index.js
+++ index.js
@@ -27,26 +27,31 @@
 //
 // useLineReturns       Use embedded line returns in cells. (Only in spreadsheet mode.)
 
-const cmd           = require('./lib/cmd')
-    , hp            = require('highland-process');
-
-module.exports = Tabula;
+const cmd = require('./lib/cmd');
+const hp = require('highland-process');
 
 function Tabula(pdfPath, options) {
-  if (!(this instanceof Tabula)) return new Tabula(pdfPath, options);
+  if (!(this instanceof Tabula)) {
+    return new Tabula(pdfPath, options);
+  }
+
   this.pdfPath = pdfPath;
   this.options = options;
 }
 
 Tabula.prototype.streamCsv = function () {
-  return hp.from(cmd(this.pdfPath, this.options).run());
+  return hp
+    .from(cmd(this.pdfPath, this.options)
+    .run());
 };
 
 Tabula.prototype.extractCsv = function (cb) {
   this.streamCsv()
-  .map(data => data.toString())
-  .split()
-  .collect()
-  .stopOnError(err => cb(err, null))
-  .each(data => cb(null, data));
+    .map(data => data.toString())
+    .split()
+    .collect()
+    .stopOnError(err => cb(err, null))
+    .each(data => cb(null, data));
 };
+
+module.exports = Tabula;
